463402e67b31147550c8fd7def85d62aaeaba312
Rodrigo Andrés Díaz Vergara <rdiaz@lemontech.cl>
2017-06-02 15:29:32 -0400

    Cleans the dist folder between builds

--- webpack/webpack.config.production.js
+++ webpack/webpack.config.production.js
@@ -1,6 +1,7 @@
 var Config = require('webpack-config').default
 var path = require('path')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = new Config().extend(path.resolve('webpack/webpack.config.base.js')).merge({
   devtool: 'source-map',
@@ -35,6 +36,10 @@ module.exports = new Config().extend(path.resolve('webpack/webpack.config.base.j
   plugins: [
     new ExtractTextPlugin({
       filename: '[name]-[contenthash].css',
+    }),
+    new CleanWebpackPlugin(['dist/*.*'], {
+      root: path.resolve('.'),
+      verbose: false,
     }),
   ],
 })
